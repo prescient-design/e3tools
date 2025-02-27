db4afdbf3140c1e06d0cf8f6099c298532f35871
Ameya Daigavane <ameya.d.98@gmail.com>
2025-02-27 01:24:39 +0000

    Fix name.

--- src/e3tools/nn/_extract_irreps.py
+++ src/e3tools/nn/_extract_irreps.py
@@ -5,21 +5,21 @@ import e3nn.o3
 class ExtractIrreps(torch.nn.Module):
     """Extracts specific irreps from a e3nn tensor."""
 
-    def __init__(self, irreps_in: e3nn.o3.Irreps, irreps_extract: e3nn.o3.Irrep):
+    def __init__(self, irreps_in: e3nn.o3.Irreps, irrep_extract: e3nn.o3.Irrep):
         super().__init__()
 
         self.irreps_in = e3nn.o3.Irreps(irreps_in)
-        self.irreps_extract = e3nn.o3.Irrep(irreps_extract)
+        self.irrep_extract = e3nn.o3.Irrep(irrep_extract)
 
         irreps_out = e3nn.o3.Irreps()
         slices = []
         for (mul, ir), ir_slice in zip(self.irreps_in, self.irreps_in.slices()):
-            if ir.l == self.irreps_extract.l and ir.p == self.irreps_extract.p:
+            if (ir.l, ir.p) == (self.irrep_extract.l, self.irrep_extract.p):
                 slices.append(ir_slice)
                 irreps_out += e3nn.o3.Irreps(f"{mul}x{ir}")
 
         if len(slices) == 0:
-            raise ValueError(f"irreps {irreps_extract} not found in {irreps_in}")
+            raise ValueError(f"Irreps {irrep_extract} not found in {irreps_in}")
 
         self.slices = slices
         self.irreps_out = irreps_out
