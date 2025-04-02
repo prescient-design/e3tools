d0c8bd7b80c9800bde9b9c1e0a004bc5ca1bf76e
Ameya Daigavane <ameya.d.98@gmail.com>
2025-04-02 19:10:58 -0400

    Fix.

--- src/e3tools/__init__.py
+++ src/e3tools/__init__.py
@@ -2,4 +2,4 @@ from ._scatter import scatter
 from ._radius import radius, radius_graph
 from ._pack_unpack import pack_irreps, unpack_irreps
 
-__all__ = ["scatter", "radius", "radius_graph"]
+__all__ = ["scatter", "radius", "radius_graph", "pack_irreps", "unpack_irreps"]
