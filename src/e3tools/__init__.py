95cbcc7883747e5507a6dafb1e56cada4b28cd18
Joseph Kleinhenz <kleinhej@gene.com>
2025-08-04 22:57:05 +0000

    lint

--- src/e3tools/__init__.py
+++ src/e3tools/__init__.py
@@ -3,10 +3,10 @@ from ._radius import radius, radius_graph
 from ._pack_unpack import pack_irreps, unpack_irreps
 
 __all__ = [
-        "pack_irreps", 
-        "radius", 
-        "radius_graph", 
-        "scatter", 
-        "scatter_softmax", 
-        "unpack_irreps"
+    "pack_irreps",
+    "radius",
+    "radius_graph",
+    "scatter",
+    "scatter_softmax",
+    "unpack_irreps",
 ]
